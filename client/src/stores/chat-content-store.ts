1fa2de46d9cad14f8451d0ff50b334b2982a11e5
ganda1fero <makk2006xx@gmail.com>
2026-03-25 13:53:35 +0300

    (client) basic implementation chat-content-store

---
+++ client/src/stores/chat-content-store.ts
@@ -0,0 +1,74 @@
+import type { UUID } from "@/types/UUID";
+import type { ChatContent } from "@/types/chatContent";
+import type { Ref } from 'vue'
+import type { GetChatContentPacket } from "@/types/web-socket/client/get-chat-content-packet";
+
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import { useWebSocketStore } from "./useWebSocketStore";
+
+
+export const useChatContentStore = defineStore('chatContent', () => {
+    // --- state
+    const chatContents = new Map<UUID, Ref<ChatContent>>(); // UUID = chat uuid
+
+    const LRUcache = new Set<UUID>();   // UUID = chat uuid
+    const MAX_LRU_COUNT = 10;
+
+    const socketStore = useWebSocketStore();
+
+    // --- getters
+    const getChatContent = (chatId: UUID): ChatContent => {
+        const existing = chatContents.get(chatId);
+        if (!existing) {
+            const newChatContent: ChatContent = {
+                messages: [],
+                chatId,
+                isLoading: true,
+                hasMoreOlder: false,
+                hasMoreNewer: false,
+            }
+
+            addChatContent(newChatContent);
+            fetchStartContent(chatId);
+            return newChatContent;
+        }
+
+        LRUcache.delete(existing.value.chatId);
+        LRUcache.add(existing.value.chatId);
+        return existing.value;
+    }
+
+    // --- actions
+    const addChatContent = (chatContent: ChatContent): boolean => {
+        if (chatContents.has(chatContent.chatId)) return false;
+
+        LRUcache.add(chatContent.chatId);
+        if (LRUcache.size >= MAX_LRU_COUNT) {
+            const firstKey = LRUcache.keys().next().value;
+            if (firstKey) {
+                LRUcache.delete(firstKey);
+                chatContents.delete(firstKey);
+            }
+        }
+
+        const refChatContent: Ref<ChatContent> = ref(chatContent);
+        chatContents.set(chatContent.chatId, refChatContent);
+        return true;
+    }
+
+    const fetchStartContent = (chatId: UUID): void => {
+        const chatContentPacket: GetChatContentPacket = {
+            type: 'GET_CHAT_CONTENT',
+            chatId,
+        };
+
+        socketStore.send(chatContentPacket);
+    }
+
+    // fetchOlder method
+
+    // fetchNewer method
+
+    return { getChatContent };
+});

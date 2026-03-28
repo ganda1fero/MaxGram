5bbfb69799b685332de08571fe810a5bd27320fe
ganda1fero <makk2006xx@gmail.com>
2026-03-28 18:07:09 +0300

    added debounce function

---
+++ client/src/services/debounce.ts
@@ -0,0 +1,15 @@
+function debounce<T extends (...args: any[]) => any>(fn: T, delayMs: number = 100) {
+    let timeoutPtr: number | null = null;
+
+    const debouncedFn = function(this: any, ...args: Parameters<T>){
+        if (timeoutPtr) clearTimeout(timeoutPtr);
+        timeoutPtr = setTimeout(() => fn.apply(this, args), delayMs);
+    }
+
+    const cancelTimeout = () => {
+        if (timeoutPtr) clearTimeout(timeoutPtr);
+        timeoutPtr = null;
+    };
+
+    return { debouncedFn, cancelTimeout };
+}

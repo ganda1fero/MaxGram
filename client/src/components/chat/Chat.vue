22e8147721d5bb4fc263f6050e8b5a32bb30ce43
ganda1fero <makk2006xx@gmail.com>
2026-03-31 18:32:02 +0300

    (client) <code refactor> moved the chat to a separate component

---
+++ client/src/components/chat/Chat.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+    import ChatInput from '@/components/chat/ChatInput.vue';
+
+</script>
+<template>
+    <div class="chat-field">
+        <div class="messages-field">
+            
+        </div>
+        <div class="input-area">
+            <ChatInput />
+        </div>
+    </div>
+</template>
+<style scoped>
+    .chat-field{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        width: 100%;
+        height: 100%;
+        padding: 0px;
+
+
+        border: solid red 1px;
+    }
+
+    .messages-field{
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+
+        overflow-y: auto;
+
+        width: 100%;
+        max-width: 750px;
+
+        border: solid blueviolet 1px;
+    }
+
+    .input-area{
+        display: inline-flex;
+        flex-shrink: 0;
+
+        width: 100%;
+        max-width: 705px;
+
+        border:solid yellow 1px;
+    }
+</style>
